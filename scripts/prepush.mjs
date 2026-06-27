41df0aed4b1335a768ed0b1cfe0749293f908fc8
RoryGlenn <32777747+RoryGlenn@users.noreply.github.com>
2026-06-27 15:28:09 -0400

    Add pre-commit checks more

---
+++ scripts/prepush.mjs
@@ -0,0 +1,4 @@
+runCommand("format check", "npm", ["run", "format:check"]);
+runCommand("lint", "npm", ["run", "lint"]);
+runCommand("tests", "npm", ["test"]);
+runCommand("build", "npm", ["run", "build"]);
