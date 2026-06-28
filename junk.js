49cb34303fd24182d0193d28182fc2d0faeca774
RoryGlenn <32777747+RoryGlenn@users.noreply.github.com>
2026-06-28 09:00:26 -0400

    Creating junk file

--- junk.js
+++ junk.js
@@ -8,8 +8,9 @@
 const unusedVariable = 42;
 const messy = "not   formatted";
 
-function add(a, b) {
-  return a + b;
+function add(a,  b  ) {
+  return a  + b;
 }
 
-console.log(add(1, 2), messy);
+console.log(
+    add(1, 2), messy);
